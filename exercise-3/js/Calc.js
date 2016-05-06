1046224234d5688ddea58beeb2d09cc3c8a1ba77
quinnmau <mauq@uw.edu>
2016-05-06 16:48:45 -0700

    calculator works

--- exercise-3/js/Calc.js
+++ exercise-3/js/Calc.js
@@ -4,8 +4,40 @@ window.Calc = (function() {
     var c = function(num) {
         // Find a way to store all instance viariables and functions
         // for this calculator
-
+        var calc = {};
+        
+        calc.number = num;
         // Implement getVal, plus, minus, times, divide, and clear
+        calc.getVal = function() {
+            return calc.number;
+        };
+        
+        calc.plus = function(value) {
+            this.number += value;
+            return this;
+        };
+        
+        calc.minus = function(value) {
+            this.number -= value;
+            return this;
+        };
+        
+        calc.times = function(value) {
+            this.number *= value;
+            return this;
+        };
+        
+        calc.divide = function(value) {
+            this.number /= value;
+            return this;
+        };
+        
+        calc.clear = function() {
+            this.number = 0;
+            return this;
+        };
+        
+        return calc;
     };
 
     // Return your constructed object
